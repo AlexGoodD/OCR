fad101bab320352b8c899e4b8fb658d98b598f47
AlexGoodD <alejandrochavarb@gmail.com>
2025-07-10 16:27:02 -0600

    Funcionalidad base terminada

--- OCR/ContentView.swift
+++ OCR/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var recognisedText = "Press start to scan"
+    @State private var showingScanningView = false
+    let buttonHeight: CGFloat = 30
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                ScrollView {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .fill(Color.white)
+                            .shadow(radius: 3)
+                        Text(recognisedText)
+                            .padding()
+                    }
+                    .padding()
+                }
+                
+                Spacer()
+                
+                HStack (spacing: 20) {
+                    Button(action:  {
+                        self.showingScanningView = true
+                    }) {
+                        Text("Start scanning")
+                            .frame(maxWidth: .infinity, minHeight: buttonHeight)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Capsule().fill(Color.blue))
+                            
+                    }
+                    
+                    Button(action:  {
+                        self.copyToClipboard()
+                    }) {
+                        Text("Start scanning")
+                            .frame(maxWidth: .infinity, minHeight: buttonHeight)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Capsule().fill(Color.green))
+                    }
+                    .disabled(recognisedText == "Press start to scan")
+                }
+                .padding()
+            }
+            .background(.gray.opacity(0.1))
+            .navigationBarTitle("Document Scanner")
+            .sheet(isPresented: $showingScanningView) {
+                ScanDocumentView(recognisedText: self.$recognisedText)
+            }
         }
-        .padding()
+    }
+    
+    private func copyToClipboard() {
+        UIPasteboard.general.string = recognisedText
     }
 }
 
