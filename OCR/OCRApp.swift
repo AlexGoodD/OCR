42bbf90fef65fb085e856a6aed93293fe5caca05
AlexGoodD <alejandrochavarb@gmail.com>
2025-07-10 15:43:45 -0600

    Initial Commit

---
+++ OCR/OCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRApp.swift
+//  OCR
+//
+//  Created by Alejandro on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct OCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
